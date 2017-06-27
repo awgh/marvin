3a2b9e5d6bb2e61938915e82398183c8ac8a631b
awgh <awgh@awgh.org>
2017-06-27 13:28:50 -0700

    Added markov dependency installation to install script.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p $1/config
-go get github.com/mattn/go-sqlite3 && go get github.com/fluffle/goirc
+go get github.com/mattn/go-sqlite3 && go get github.com/fluffle/goirc && go get github.com/awgh/markov
 cd $1 && go build github.com/awgh/marvin/marvin && cp $GOPATH/src/github.com/awgh/marvin/db/* . && cp $GOPATH/src/github.com/awgh/marvin/config/* .
 
