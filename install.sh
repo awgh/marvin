23df61d4a356b8fec4dc07c10ffd019942cb05f6
awgh <awgh@awgh.org>
2019-06-15 15:39:59 -0700

    Added Slack lib to installer script.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 GP=`go env GOPATH`
 
 mkdir -p $1/config
-go get github.com/mattn/go-sqlite3 && go get github.com/fluffle/goirc && go get github.com/awgh/markov
+go get github.com/mattn/go-sqlite3 && go get github.com/fluffle/goirc && go get github.com/awgh/markov && go get github.com/nlopes/slack
 cd $1 && go build github.com/awgh/marvin && cp $GP/src/github.com/awgh/marvin/db/* . && cp $GP/src/github.com/awgh/marvin/config/* .
 
