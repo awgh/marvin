ead480d7ea72f53b84ea96e871312e875dccc994
Aw Gh <awgh@awgh.org>
2020-02-21 15:43:49 -0800

    Improved link handling for tweet preview.

--- marvin/twitter.go
+++ marvin/twitter.go
@@ -16,38 +16,35 @@ func CheckForTweet(text string) (bool, string) {
 }
 
 // GetTweetFromURL - fetches a tweet
-func GetTweetFromURL(url string) (string, string) {
+func GetTweetFromURL(url string) string {
 	c := colly.NewCollector(
 		colly.AllowedDomains("mobile.twitter.com"),
 		colly.UserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"),
 	)
 
+	idx := strings.LastIndexByte(url, '/')
+	dataID := url[idx+1:]
+
 	text := ""
-	href := ""
-	c.OnHTML("div.tweet-text div.dir-ltr", func(e *colly.HTMLElement) {
-		if text != "" {
+	c.OnHTML("div.tweet-text", func(e *colly.HTMLElement) {
+		if text != "" || e.Attr("data-id") != dataID {
 			return
 		}
 		text = strings.Trim(string(e.Text), " \n\t\v")
-	})
-	c.OnHTML("div.tweet-text div.dir-ltr a[href]", func(e *colly.HTMLElement) {
+		href := e.ChildAttr("a[href]", "href")
 		if href != "" {
-			return
+			text = text + " " + href
 		}
-		href = strings.Trim(e.Attr("href"), " \n\t\v")
 	})
 	c.Visit(url)
 
-	return text, href
+	return text
 }
 
 // GetTweetFromText - extracts tweet links from text and fetches the tweet
 func GetTweetFromText(text string) string {
 	if ok, s := CheckForTweet(text); ok {
-		tweet, href := GetTweetFromURL("https://mobile." + s)
-		if href != "" {
-			return tweet + " " + href
-		}
+		tweet := GetTweetFromURL("https://mobile." + s)
 		return tweet
 	}
 	return ""
